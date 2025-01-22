2558cb6b8f93f98a3f2e1476a4fccc909daf9bfb
Roberto Carrera-Martínez <78400990+rcarrmart@users.noreply.github.com>
2025-01-22 02:03:49 -0600

    Add files via upload

---
+++ SLDivMaxR/R/z.R
@@ -0,0 +1,5 @@
+#' @export
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to SLDivMaxR! :) \n     version 0.1.0 \n
+Learn more at https://github.com/rcarrmart/SLDivMaxR \n")
+}
