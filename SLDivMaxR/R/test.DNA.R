4d5c638582cda67d404cacbdd5bd602ba6ba0c46
Roberto Carrera-Martínez <78400990+rcarrmart@users.noreply.github.com>
2025-02-07 00:49:32 -0600

    Add files via upload

---
+++ SLDivMaxR/R/test.DNA.R
@@ -0,0 +1,6 @@
+#' test.DNA
+#'
+#' Fake DNA sequences to test funtions
+#'
+#' @format A list of nine randomly simulated DNA dataset of three specimens for three "species", composed of 50 bp each.
+#'
