ca9ddc1228bf038e1f14e8aa6405e45c86cc1e3c
Roberto Carrera-Martínez <78400990+rcarrmart@users.noreply.github.com>
2025-02-08 16:40:03 -0600

    Update DNA.Dist.df.R
    Update examples and function documentation

--- SLDivMaxR/R/DNA.Dist.df.R
+++ SLDivMaxR/R/DNA.Dist.df.R
@@ -2,30 +2,63 @@
 #'
 #' A function that applies `ape::dist.dna()` but changes `"dist"` class output from `ape::dist.dna()` (or similar data) to `data.frame`.
 #'
-#' @param inDNA the input aligned sequences in `.fasta` format.
+#' @param inDNA The input aligned sequences in fasta format.
 #'
-#' @param Dist.type specifies if the distance output should be in proportions (`"prop"`), percentages (`"percentage"`), or number of nucleotides that are different (`"nucleotide"`).
+#' @param Dist.type Specifies the distance output format. Options are: `"prop"`: Proportions; `"percentage"`: Percentages; and `"nucleotide"`: Number of nucleotides that are different.
 #'
-#' @param Model specifies the evolutionary model to use. Same as in `ape::dist.dna`. Options are: `"RAW"`, `"JC69"`, `"K80"` (default), `"F81"`, `"K81"`, `"F84"`, `"T92"`, `"TN93"`, `"GG95"`, `"LOGDET"`, `"BH87"`, `"PARALIN"`, `"N"`, `"TS"`, `"TV"`, `"INDEL"`, and `"INDELBLOCK"`. Note: if `Model` is `"N"` and `Dist.type` is set for `"prop"` or `"percentage"`, then an error will be return.
+#' @param Model Specifies the evolutionary model to use, as in `ape::dist.dna`. Options include: `"RAW"`, `"JC69"`, `"K80"` (default), `"F81"`, `"K81"`, `"F84"`, `"T92"`, `"TN93"`, `"GG95"`, `"LOGDET"`, `"BH87"`, `"PARALIN"`, `"N"`, `"TS"`, `"TV"`, `"INDEL"`, and `"INDELBLOCK"`. Note: If `Model = "N"` and `Dist.type` is set to `"prop"` or `"percentage"`, an error will be returned.
 #'
-#' @param GAMMA same as `gamma` in `ape::dist.dna()`.
+#' @param GAMMA Same as the `gamma` parameter in `ape::dist.dna()`.
 #'
-#' @param PW.deletion specifies if Pairwise deletion should be considered (`TURE`, default) or not (`FALSE`).
+#' @param PW.deletion Specifies whether pairwise deletion should be considered (`TRUE`, default) or not (`FALSE`).
 #'
-#' @param Var if `TRUE` output is the calculated variation (`FALSE` is default)
+#' @param Var If `TRUE`, the output will include calculated variation. Default is `FALSE`.
 #'
-#' @param by.group a vector to group sequences on different categories (e.g., populations, species, lineages, etc.). The vector should follow the same order as the sequence alignment, by applying `vegan::meandist()`. The vector can be the `.csv` output of species delimitation analyses such as `ASAP` and `ABGD`.
+#' @param by.group A vector for grouping sequences into different categories (e.g., populations, species, lineages). The order should match the sequence alignment and be used with `vegan::meandist()`.
 #'
-#' @param `group.summary` applies `vegan::meandist()` to formulate a data.frame with within groups mean distance, between groups mean distance and overall distance. A vector with discreet groupings should be imported in `by.group`.
+#' @param group.summary Applies `vegan::meandist()` to generate a `data.frame` with within-group mean distance, between-group mean distance, and overall distance. A vector with discrete groupings should be provided in `by.group`.
 #'
-#' @param `within.group` applies `vegan::meandist()` to create a `data.frame` with each within group mean divergence, and number of samples ("N") by group. A vector with discreet groupings should be imported in `by.group`. Groupls with N = 1 will return `NA`'s.
+#' @param within.group Applies `vegan::meandist()` to create a `data.frame` with each group’s mean divergence and the number of samples ("N") by group. A vector with discrete groupings should be provided in `by.group`. Groups with `N = 1` will return `NA`s.
 #'
 #' @returns A dataframe with the mean distance between groups or within group distances.
 #'          Note: `DNA.Dist.df` should not be used as a species delimitation analysis as it does not explicitly tests a hypothesis.
 #'
 #' @examples
-#' library("ape")
-#' #UNder construction!
+#' # Distance matrix class == data.frame of all specimens
+#' DNA.Dist.df(test.DNA, Out.Format = "wide")
+#'
+#' # same as above but with JC96 model
+#' DNA.Dist.df(test.DNA, Out.Format = "wide", Model = "JC69")
+#'
+#' # as above, but long pair-wise comparison format
+#' DNA.Dist.df(test.DNA, Out.Format = "long", Model = "JC69")
+#'
+#' ####
+#' # Defining groupings
+#' ####
+#'
+#' #create data.frame with specimen and grouping (e.g., species) IDs
+#' Species.info <- data.frame(Specimen = paste0("Ind",LETTERS[1:9]),
+#'                            Species = paste0("Sp",c(rep(1,3),rep(2,3),rep(3,3))),
+#'                            Seq.names = names(test.DNA))
+#'
+#' Species.info  # Check that they match.
+#' # The funtion uses the order of the `by.group` vector to group the sequences
+#'
+#' DNA.Dist.df(test.DNA, Out.Format = "long", Model = "JC69", by.group = Species.info$Species)
+#'
+#' # Reorganize the output
+#' Comparison <- DNA.Dist.df(test.DNA, Out.Format = "long", Model = "JC69",
+#'                           by.group = Species.info$Species)
+#' Comparison <- data.frame(Pairs = paste(Comparison$x1, Comparison$x2, sep = "_vs_"),
+#'                          Distance = Comparison$Distance)
+#' Comparison
+#'
+#' #wide format
+#' WDist <- DNA.Dist.df(test.DNA, Out.Format = "wide", Model = "JC69",
+#'                       by.group = Species.info$Species)
+#' WDist
+#'
 #'
 #' @export
 DNA.Dist.df <- function(inDNA,
@@ -42,8 +75,11 @@ DNA.Dist.df <- function(inDNA,
   MODELS <- c("RAW", "JC69", "K80", "F81", "K81", "F84", "T92", "TN93", "GG95", "LOGDET", "BH87", "PARALIN", "N", "TS",
               "TV", "INDEL", "INDELBLOCK")
   imod <- pmatch(toupper(Model), MODELS)
-  if (is.na(imod)) stop(paste("'Model' must be one of:", paste("\"", MODELS,"\"", sep = "", collapse = " "),
+  if (is.na(imod))  {
+                    cat("Model provided:", Model, "\n")
+                    stop(paste("'Model' must be one of:", paste("\"", MODELS,"\"", sep = "", collapse = " "),
                               ". See ape::dist.dna for more information.", sep = ""))
+                    }
 
   DT <- c("percent", "prop", "nucleotide")
   iDT <- pmatch((Dist.type), DT)
@@ -51,6 +87,8 @@ DNA.Dist.df <- function(inDNA,
 
   if (Dist.type != "nucleotide" & Model == "N")
     stop("Change 'Dist.type' to 'nucleotide' if input for Model is 'N'.")
+  if (length(by.group) != length(inDNA)) stop("`by.group` vector and the DNA list are of different lengths.")
+  if (is.na(by.group) == TRUE) stop("Missing data on `by.group` vector")
 
   dist.fx <- function(model, distType) {
     if (distType == "percent" && model != "N") {
